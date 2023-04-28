dda44fad176cd16e59b0be86e46f30ab94631fcd
nika-chkhaidze <nikachkhaidze08@gmail.com>
2023-04-28 17:27:07 +0400

    update1

--- main.js
+++ main.js
@@ -72,14 +72,35 @@ prevBtn.addEventListener('click', moveToPreviousSlide);
 startSlide();
 
 
-$('#slick-slider').slick({
-  autoplay: true,
-  dots: true,
-  appendArrows: $('.slick-slider-nav'),
-  appendDots: $('.slick-slider-dots'),
-  prevArrow: "<button class='slick-prev btn btn-white rounded-circle'><i class='mdi mdi-chevron-left'></i></button>",
-  nextArrow: "<button class='slick-next btn btn-white rounded-circle'><i class='mdi mdi-chevron-right'></i></button>",
-});
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
 
 
 
